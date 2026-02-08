1f7b5450f2aa78b4c8aff1ed9a5b942fe7039f6c
Líbal, Rudolf <libalrud@fel.cvut.cz>
2026-02-08 15:55:55 +0100

    fixed socket

---
+++ server_frontend.js
@@ -0,0 +1,11 @@
+﻿/*TUTORIAL CONTENT*/
+
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+app.use(express.static('public'))
+
+http.listen(3001, () => {
+    console.log('listening on *:3001')
+})
